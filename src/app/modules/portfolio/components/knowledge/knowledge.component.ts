908844e48c26c4650b9602594cae8d0d05404303
Poliana <poccos@unifei.edu.br>
2025-05-18 20:33:01 -0300

    Modificando header

---
+++ src/app/modules/portfolio/components/knowledge/knowledge.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-knowledge',
+  imports: [],
+  templateUrl: './knowledge.component.html',
+  styleUrl: './knowledge.component.scss'
+})
+export class KnowledgeComponent {
+
+}
